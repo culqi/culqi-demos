6b4fbc0221fdf06862cd7866b9d3edb0f56558bd
Brando Javier  Carquin Mendocilla <brando.carquin@culqi.com>
2025-01-02 17:07:13 -0500

    new arq rust

---
+++ src/client/credentials.rs
@@ -0,0 +1,11 @@
+#[allow(dead_code)]
+pub const PUBLIC_KEY: &str = "pk_test_90667d0a57d45c48";
+
+#[allow(dead_code)]
+pub const SECRET_KEY: &str = "sk_test_1573b0e8079863ff";
+
+#[allow(dead_code)]
+pub const RSA_ID: &str = "30b83fd0-8709-4fe4-86c1-fef042c3c2c3";
+
+#[allow(dead_code)]
+pub const RSA_KEY: &str = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDuCmwMoEzvBk++m4rZUlZL4pDD\nW++NV1tSjAOJsRv5Ermg3/ygjINNhi1gfMbfSiWloc85tJBZhXzD7JpOd7JxOOg7\nCicgbZKGF/sq2geoVw4+n4j4vUZx0+a1PgStwR+BeZn2I+eAn9xOrHJD6/baJqIO\n/ifGJ1e5jHeQXIR4IwIDAQAB\n-----END PUBLIC KEY-----";
